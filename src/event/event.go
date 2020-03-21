6fc804c0b31c4816ea534bb067ce9a1d92b2a4e3
Vladimir Borodin <boberneprotiv@gmail.com>
2020-03-21 22:00:03 +0200

    Add constructor for event

--- src/event/event.go
+++ src/event/event.go
@@ -1,5 +1,20 @@
 package event
 
+import "time"
+
 type Event struct {
-	Code int
+	Code int       `json:"code"`
+	Time time.Time `json:"time"`
 }
+
+func NewEvent(code int) Event {
+	return Event{
+		Code: code,
+		Time: time.Now(),
+	}
+}
+
+const (
+	GameStart = 2048
+	GameEnd   = 2049
+)
